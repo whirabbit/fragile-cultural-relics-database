72004736ac3ed56e6389460cfecc2570c32ca3c7
whirabbit <2324127579@qq.com>
2022-02-28 15:54:13 +0800

    Initial commit

---
+++ src/main/java/com/fehead/fragileculturalrelicsdatabase/FragileCulturalRelicsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.fehead.fragileculturalrelicsdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragileCulturalRelicsDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragileCulturalRelicsDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fehead/fragileculturalrelicsdatabase/FragileCulturalRelicsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fehead.fragileculturalrelicsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragileCulturalRelicsDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
